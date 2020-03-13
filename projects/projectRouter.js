5e2ee4ef2e67a898bf5897c259dfc9c1ba9bee23
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-13 10:58:19 -0500

    projects Router is now working with all CRUD operations

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -9,10 +9,10 @@ router.get("/", (req, res) => {
 projects.get()
 .then(projects => {
     res.status(200).json(projects)
-})
+    })
 .catch(err => {
     res.status(500).json({message: err})
-})
+    })
 })
 
 router.get("/:id", (req, res) => {
@@ -25,6 +25,45 @@ router.get("/:id", (req, res) => {
     })
 })
 
+router.get("/:id/actions", (req, res) => {
+    projects.getProjectActions(req.params.id)
+    .then(projaction => {
+        res.status(200).json(projaction)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
+router.post("/", (req, res) => {
+    projects.insert(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
+router.put("/:id", (req, res) => {
+    projects.update(req.params.id, req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    projects.remove(req.params.id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
 
 
 module.exports = router; 
